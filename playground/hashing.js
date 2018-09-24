fd37deee393e8a9d1f0c188f824fa2cb11a62efd
wing <eternity-wing>
2018-09-24 07:04:22 +0330

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const  {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt)  => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
+var hashedPassword = "$2a$10$gBud0cUzEvqppYrLACZH/uJWDuWX20STAh.IFIQn9KvvzlZKyhWbm";
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
-console.log('decoded: ', decoded);
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+
+// console.log('decoded: ', decoded);
 
 
 // var message = 'I am user number 3';
